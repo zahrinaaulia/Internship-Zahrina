7dc943cd89d254b91be30969aa715675eaa3a72d
zahrinaadriani <zahrinaadriani@gmail.com>
2020-06-02 20:29:42 +0700

    First Commit

---
+++ resources/views/mahasiswa.blade.php
@@ -0,0 +1,13 @@
+@extends('layout/main')
+
+@section('title','Mahasiswa')
+
+@section('container')
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-10">
+          <h1 class="mt-3">Hello, Mahasiswa</h1>
+        </div>
+    </div>
+  </div>
+@endsection
